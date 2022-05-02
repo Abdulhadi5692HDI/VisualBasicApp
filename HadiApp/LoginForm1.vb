1ffa3c5cecc2adbeb70100017a0bc43653761a31
Abdulhadi5692HDI <87948364+Abdulhadi5692HDI@users.noreply.github.com>
2022-05-02 15:42:43 +0100

    HadiApp 1.0.0

---
+++ HadiApp/LoginForm1.vb
@@ -0,0 +1,21 @@
+﻿Public Class LoginForm1
+
+    ' Sets Username and Password '
+
+        Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        If UsernameTextBox.Text = "hadi" Then
+            Form2.Show()
+            Me.Close()
+        End If
+        If UsernameTextBox.Text = "*" Then
+            ErrorDialog1.Show()
+            Me.Close()
+        End If
+    End Sub
+
+        Private Sub Cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel.Click
+            Form1.Show()
+            Me.Close()
+        End Sub
+
+    End Class
